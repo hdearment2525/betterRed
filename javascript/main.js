32a1be0648c5dd09738e69cef7cb2e24e1449463
Hunter DeArment <hdearment25@gmail.com>
2023-07-14 15:51:09 -0600

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow} = require('electron')
+
+let splash;
+let mainWindow;
+
+function createWindow() {
+    // create the splash window
+    splash = new BrowserWindow({width: 805, height: 605, transparent: true, frame: false, alwaysOnTop: true});
+    splash.loadFile('html/splash.html');
+
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: `${__dirname}/bettered-logo.bmp`,
+        show: false // this will hide the main window until its content is loaded
+    })
+
+    mainWindow.loadFile('html/index.html');
+
+    // when the content has finished loading, show the window and hide the splash screen
+    mainWindow.once('ready-to-show', () => {
+        setTimeout(() => {
+            splash.destroy();
+            mainWindow.show();
+        }, 5000); // 5 seconds delay
+    });
+}
+
+app.whenReady().then(createWindow);
